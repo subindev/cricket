12103507fb90a2e39a0d455431d6d315e2b3f938
subindev <subinpdevs@gmail.com>
2016-02-19 18:08:24 +0400

    'fixtures'

--- www/app/app.js
+++ www/app/app.js
@@ -3,22 +3,61 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('cricketApp', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
+
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'app/layout/menu.html',
+   // controller: 'AppCtrl'
+  })
+
+  .state('app.fixtures', {
+    url: '/fixtures',
+    views: {
+      'menuContent': {
+        templateUrl: 'app/fixtures/fixtures.html'
+      }
+    }
+  })
+
+  .state('app.teams', {
+      url: '/teams',
+      views: {
+        'menuContent': {
+          templateUrl: 'app/teams/teams.html'
+        }
+      }
+    })
+    .state('app.favorites', {
+      url: '/favorites',
+      views: {
+        'menuContent': {
+          templateUrl: 'app/favorites/favorites.html',
+          //controller: 'PlaylistsCtrl'
+        }
+      }
+    });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/fixtures');
+});
